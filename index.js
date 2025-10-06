a8876d691823940f15dd4684d6151d80d552c325
vijay810 <vijay951mane@gail.com>
2025-10-06 12:47:52 +0530

    Initial commit

--- index.js
+++ index.js
@@ -12,6 +12,7 @@ const authRoutes = require('./routes/auth.routes')
 const leavesRoutes = require('./routes/leave.routes')
 const newsRoutes = require('./routes/news.routes')
 const teamsRoutes = require('./routes/teams.routes')
+const eventsRoutes = require('./routes/events.routes')
 
 const app = express();
 
@@ -30,6 +31,7 @@ app.use('/clients', clientsRoutes);
 app.use('/leave', leavesRoutes);
 app.use('/news', newsRoutes);
 app.use('/teams', teamsRoutes)
+app.use('/events', eventsRoutes)
 // PORT
 const port = process.env.PORT || 4000;
 app.listen(port, () => {

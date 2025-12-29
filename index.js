cb4860b0ad4d40efc3bac08120eee2980068c2d5
vijay810 <vijay951mane@gmail.com>
2025-12-29 12:07:12 +0530

    update

--- index.js
+++ index.js
@@ -1,19 +1,72 @@
+// require('dotenv').config();
+
+// const express = require('express');
+// const bodyParser = require('body-parser');
+// const cors = require('cors');
+// const connectDB = require('./config/db');
+// // const studentRoute = require('./routes/student.routes');
+// // const schoolRoute = require('./routes/school.routes');
+// const userRoute = require('./routes/user.routes');
+// const clientsRoutes = require('./routes/clients.routes')
+// const authRoutes = require('./routes/auth.routes')
+// const leavesRoutes = require('./routes/leave.routes')
+// const newsRoutes = require('./routes/news.routes')
+// const teamsRoutes = require('./routes/teams.routes')
+// const eventsRoutes = require('./routes/events.routes')
+// const smsRoutes = require('./routes/sms.routes')
+
+// const app = express();
+
+// // Connect to MongoDB
+// connectDB();
+
+// // Middleware
+// app.use(bodyParser.json());
+// app.use(bodyParser.urlencoded({ extended: true }));
+// app.use(cors());
+
+// // Routes
+// app.use('/auth', authRoutes);
+// app.use('/user', userRoute);
+// app.use('/clients', clientsRoutes);
+// app.use('/leave', leavesRoutes);
+// app.use('/news', newsRoutes);
+// app.use('/teams', teamsRoutes)
+// app.use('/events', eventsRoutes)
+// app.use('/sms', smsRoutes)
+// // PORT
+// const port = process.env.PORT || 4000;
+// app.listen(port, () => {
+//     console.log(`Server is running on port ${port}`);
+// });
+
+
+// // 404 Error
+// app.use((req, res, next) => {
+//     res.status(404).send('Not Found');
+// });
+
+// // Global Error Handler
+// app.use((err, req, res, next) => {
+//     console.error(err.message);
+//     res.status(err.statusCode || 500).send(err.message);
+// });
+
+
 require('dotenv').config();
 
 const express = require('express');
 const bodyParser = require('body-parser');
 const cors = require('cors');
 const connectDB = require('./config/db');
-// const studentRoute = require('./routes/student.routes');
-// const schoolRoute = require('./routes/school.routes');
 const userRoute = require('./routes/user.routes');
-const clientsRoutes = require('./routes/clients.routes')
-const authRoutes = require('./routes/auth.routes')
-const leavesRoutes = require('./routes/leave.routes')
-const newsRoutes = require('./routes/news.routes')
-const teamsRoutes = require('./routes/teams.routes')
-const eventsRoutes = require('./routes/events.routes')
-const smsRoutes = require('./routes/sms.routes')
+const clientsRoutes = require('./routes/clients.routes');
+const authRoutes = require('./routes/auth.routes');
+const leavesRoutes = require('./routes/leave.routes');
+const newsRoutes = require('./routes/news.routes');
+const teamsRoutes = require('./routes/teams.routes');
+const eventsRoutes = require('./routes/events.routes');
+const smsRoutes = require('./routes/sms.routes');
 
 const app = express();
 
@@ -31,15 +84,9 @@ app.use('/user', userRoute);
 app.use('/clients', clientsRoutes);
 app.use('/leave', leavesRoutes);
 app.use('/news', newsRoutes);
-app.use('/teams', teamsRoutes)
-app.use('/events', eventsRoutes)
-app.use('/sms', smsRoutes)
-// PORT
-// const port = process.env.PORT || 4000;
-// app.listen(port, () => {
-//     console.log(`Server is running on port ${port}`);
-// });
-
+app.use('/teams', teamsRoutes);
+app.use('/events', eventsRoutes);
+app.use('/sms', smsRoutes);
 
 // 404 Error
 app.use((req, res, next) => {
@@ -52,4 +99,14 @@ app.use((err, req, res, next) => {
     res.status(err.statusCode || 500).send(err.message);
 });
 
+// **Conditional listen**
+if (process.env.NODE_ENV !== 'production') {
+    // Local development
+    const port = process.env.PORT || 4000;
+    app.listen(port, () => {
+        console.log(`Server running on port ${port}`);
+    });
+}
+
+// Export for Vercel (production)
 module.exports = app;

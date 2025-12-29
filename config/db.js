f1032e219036b034912614e842870ce60c6bd71f
vijay810 <vijay951mane@gmail.com>
2025-12-29 12:18:21 +0530

    update

--- config/db.js
+++ config/db.js
@@ -1,13 +1,40 @@
+// const mongoose = require('mongoose');
+
+// const connectDB = async () => {
+//     try {
+//         const connection = await mongoose.connect(process.env.MONGO_URL);
+//         console.log(`Connected to Mongo! Database name: "${connection.connections[0].name}"`);
+//     } catch (err) {
+//         console.error('Error connecting to mongo', err.message);
+//         process.exit(1); 
+//     }
+// };
+
+// module.exports = connectDB;
+
+
+
+// config/db.js
 const mongoose = require('mongoose');
 
+let cached = global.mongoose;
+
+if (!cached) cached = global.mongoose = { conn: null, promise: null };
+
 const connectDB = async () => {
-    try {
-        const connection = await mongoose.connect(process.env.MONGO_URL);
-        console.log(`Connected to Mongo! Database name: "${connection.connections[0].name}"`);
-    } catch (err) {
-        console.error('Error connecting to mongo', err.message);
-        process.exit(1); 
+    if (cached.conn) return cached.conn;
+
+    if (!cached.promise) {
+        cached.promise = mongoose.connect(process.env.MONGO_URI, {
+            useNewUrlParser: true,
+            useUnifiedTopology: true
+        }).then(mongoose => mongoose);
     }
+
+    cached.conn = await cached.promise;
+    console.log(`Connected to Mongo! Database name: "${cached.conn.connections[0].name}"`);
+    return cached.conn;
 };
 
 module.exports = connectDB;
+
